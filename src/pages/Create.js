ff318d3e31f46a21401124cd139f9249ed4ab524
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-10-23 15:28:21 -0700

    create theme and create notes button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+
+})
 
 export default function Create() {
+  const classes = useStyles()
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+
+    </Container>
   )
 }
