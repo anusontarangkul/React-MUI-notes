2df9b4bd58df86535c469d961c4386394ef13831
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-10-24 23:38:40 -0700

    add active class

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 import Drawer from "@material-ui/core/Drawer"
 import Typography from "@material-ui/core/Typography"
 
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+
 import { makeStyles } from '@material-ui/core'
+
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
 
 const drawerWidth = 240
 
@@ -20,10 +28,28 @@ const useStyles = makeStyles({
     drawerPaper: {
         width: drawerWidth,
     },
+    active: {
+        background: '#f4f4f4'
+    }
 })
 
 export default function Layout({ children }) {
     const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
+
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color="secondary" />,
+            path: '/'
+        },
+        {
+            text: 'Create Note',
+            icon: <AddCircleOutlineOutlined color="secondary" />,
+            path: '/create'
+        }
+    ]
 
     return (
         <div className={classes.root}>
@@ -41,6 +67,19 @@ export default function Layout({ children }) {
                         Ninja Notes
           </Typography>
                 </div>
+                <List>
+                    {menuItems.map(item => (
+                        <ListItem
+                            button
+                            key={item.text}
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname == item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}></ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
 
                 {/* links/list section */}
 
